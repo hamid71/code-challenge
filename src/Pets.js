07c18d6a47302c9244db4c427f76de12911c6153
hamid71 <hamid.algailany@yahoo.com>
2023-07-31 13:52:29 +1000

    Adding try catch block to handle 404 no data

--- src/Pets.js
+++ src/Pets.js
@@ -1,30 +1,38 @@
-import React from 'react'
-import { petsData } from './petsData';
+import React from "react";
+import { petsData } from "./petsData";
 export const Pets = () => {
-   
-  return (
-    <>
-        <h2 className='pets-container'>Welcome to Pets owner Gender page</h2>
-        {petsData.map((data,key)=>(
-                <div key={key}>
-                    { data.pets && <h3> Owner Gender: { data.gender}</h3>}
-                    <ul>
-                        {data.pets?.sort((a,b)=>a.name.localeCompare(b.name)).map((pet,key)=>{
-                           return(
-                            <li key={key}>
-                               {pet.type==="Cat" && <span>Cat Name: {"   "}<b>{pet.name}</b></span>}
-                            </li>
-                           ) 
-                        }
-                            
-                        )}
-                    </ul>
-                  
-                </div>
-              
+  try {
+    return (
+      <>
+        <h2 className="pets-container">Welcome to Pets owner Gender page</h2>
+        {petsData.map((data, key) => (
+          <div key={key}>
+            {data.pets && <h3> Owner Gender: {data.gender}</h3>}
+            <ul>
+              {data.pets
+                ?.sort((a, b) => a.name.localeCompare(b.name))
+                .map((pet, key) => {
+                  return (
+                    <li key={key}>
+                      {pet.type === "Cat" && (
+                        <span>
+                          Cat Name: {"   "}
+                          <b>{pet.name}</b>
+                        </span>
+                      )}
+                    </li>
+                  );
+                })}
+            </ul>
+          </div>
         ))}
-    </>
-    
-  )
-}
-
+      </>
+    );
+  } catch (error) {
+    return (
+      <>
+        <h2 className="pets-container">404 No data available!</h2>
+      </>
+    );
+  }
+};
